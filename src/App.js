b1c26a77b90b17bbb1795b88c207b8ab10903ab8
dilanakbiyik <dilanakbiyik@github.com>
2018-09-09 18:55:57 +0200

    Add cards to layout. Add Roboto to index.html Use material ui design for cards.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React, {Component} from 'react';
 import { getNewsArticles } from './services/fashion-united.graphql.services';
 import './App.css';
+import Grid from '@material-ui/core/Grid';
+
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 
 const DEFAULT_IMAGE =
     'https://fashionunited.info/global-assets/img/default/fu-default_1200x630_black-favicon.jpg';
 
+const styles = theme => ({
+    media: {
+        height: 180,
+    },
+    root: {
+        padding: 16
+    },
+    card: {
+        height: 360,
+        justifyContent: 'space-between',
+        display: 'flex',
+        flexDirection: 'column'
+    }
+});
 
 class App extends Component {
     constructor(props) {
@@ -23,25 +47,50 @@ class App extends Component {
             newsArticles: result.fashionunitedNlNewsArticles,
         });
     }
+    openLink(url) {
+        window.open(url,'_blank')
+    }
     newsArticles() {
+        const { classes } = this.props;
         return this.state.newsArticles.map((newsArticle, index) =>
-            <div key={index}>
-                <h2>{newsArticle.title}</h2>
-                <img src={newsArticle.imageUrl || DEFAULT_IMAGE} />
-                <div>{newsArticle.description} <a href={newsArticle.url}>Read more</a></div>
-            </div>
+            <Grid key={index} item xs={3}>
+                <Card className={classes.card}>
+                    <CardActionArea>
+                        <CardMedia
+                            className={classes.media}
+                            image={newsArticle.imageUrl || DEFAULT_IMAGE}
+                            title={newsArticle.title}
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="headline" component="h2">
+                                {newsArticle.title}
+                            </Typography>
+                            <Typography component="p">
+                                {newsArticle.description}
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                        <Button size="small" color="primary" onClick={() => this.openLink(newsArticle.url)}>
+                            Read More
+                        </Button>
+                    </CardActions>
+                </Card>
+            </Grid>
         )
     }
     render() {
         return (
             <div className="App">
-                <h1>Fashion News</h1>
-                <div>
+                <Typography variant="display3" gutterBottom>
+                    Fashion News
+                </Typography>
+                <Grid container justify="center" spacing={16} wrap="wrap">
                     {this.newsArticles()}
-                </div>
+                </Grid>
             </div>
         );
     }
 }
 
-export default App;
+export default withStyles(styles)(App);
